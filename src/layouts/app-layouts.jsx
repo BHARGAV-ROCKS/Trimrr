9daffba9835cf8400f6932c75352cb298520a0ab
BHARGAV RAGHURAM <bhargavraghuram@gmail.com>
2024-12-28 18:04:30 +0530

    app layout

---
+++ src/layouts/app-layouts.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+
+import Header from "@/components/header"
+import { Outlet } from "react-router-dom"
+
+const AppLayout = () => {
+  return (
+    <div>
+        <main className="min-h-screen container">
+            {/* header */}
+           <Header/>
+            {/* body */}
+            <Outlet/>
+        </main>
+        {/* footer */}
+        <div className="p-10 text-center bg-gray-800 mt-10">
+        Made with 💖 by Bhargav
+        </div>
+    </div>
+  )
+}
+
+export default AppLayout
